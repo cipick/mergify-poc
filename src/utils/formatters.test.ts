9e55b79ca95315b466b0b7e8fd8d363244d19238
Ciprian Platica <platica.ciprian@gmail.com>
2026-02-28 15:43:47 +0200

    fix: remove accidentally merged failing test from main

--- src/utils/formatters.test.ts
+++ src/utils/formatters.test.ts
@@ -53,7 +53,4 @@ describe('capitalize', () => {
     expect(capitalize('')).toBe('');
   });
 
-  it('THIS TEST WILL FAIL - wrong expected value', () => {
-    expect(capitalize('hello')).toBe('WRONG');
-  });
 });
